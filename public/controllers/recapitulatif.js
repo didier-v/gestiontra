0589fee1e620df09fd8d0d60b47cac477825f10a
Didier Vandekerckhove <didierv@mac.com>
2014-01-15 15:03:21 +0100

    recapitulatif -  editing and updating the grid

--- public/controllers/recapitulatif.js
+++ public/controllers/recapitulatif.js
@@ -1,5 +1,5 @@
-/* recapitulatif.js */
-/*global _ */
+/*global _, angular */
+//recapitulatif.js
 function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 
 //notifications
@@ -64,15 +64,23 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 		return total;
 	},
 
+	$scope.compterTRAEnlever = function(dataFrais, mois) {
+		var total=0;
+		for (var i = 0; i < dataFrais.length; i++) {
+			if(dataFrais[i].date_frais!==undefined) {
+				if (DateUtils.iso2date(dataFrais[i].date_frais).getMonth() == (mois - 1)) {
+					total+=dataFrais[i].tr_a_enlever;
+				}
+			}
+		}
+		return total;
+	},
+	
 // fetch
 	$scope.fetchRecap = function() {
 		$scope.listeRecap=[];
-		//var resourceConge= dataSource({nature:"conges"});
-		//var resourceFrais = dataSource({nature:"frais"});
-		//var resourceRecap = dataSource({nature:"recapGlobal"});
-		//var resourceJourFerie = dataSource({nature:"jour_ferie"});
 		var resourceConge= $scope.resourceConge;
-		var resourceFrais = $scope.resourceFrais
+		var resourceFrais = $scope.resourceFrais;
 		var resourceRecap = $scope.resourceRecap;
 		var resourceJourFerie = $scope.resourceJourFerie;
 
@@ -81,10 +89,10 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 			for(var i=0;i<jours_feries.length;i++) {
 				jours_feries[i].doy = DateUtils.getDOY(jours_feries[i].jour);
 			}
-			var conges = resourceConge.get(
+			resourceConge.get(
 				{id_personne: $scope.personne.id,date_fin: { $regex: $scope.annee +'.*'}},
 				function(dataConges) {
-					var frais=resourceFrais.get(
+					resourceFrais.get(
 						{id_personne:$scope.personne.id, date_frais: { $regex: $scope.annee +'.*'}},
 						function(dataFrais){
 							resourceRecap.get(
@@ -93,11 +101,16 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 								var tmois=["Report","Janvier", "Février","Mars","Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 								for (var mois=0;mois<=12;mois++) {
 									var ligne={}; // ligne du tableau recap
-									ligne.mois=tmois[mois];
+									if (ligne.mois===undefined) {
+										ligne.mois=mois;
+									}
+									ligne.id_personne = $scope.personne.id;
+									ligne.annee = $scope.annee;
+									ligne.mois_texte=tmois[mois];
 									ligne.annee = Selection.annee();
 	
 									if(mois!==0) { // sauf pour la ligne report
-										ligne=angular.extend(ligne,_.findWhere(listeRecap,{mois:mois+""}));									ligne.mois=tmois[mois];
+										ligne=angular.extend(ligne,_.findWhere(listeRecap,{mois:mois}));
 										ligne.jours_ouvres=$scope.joursOuvrables(new Date(ligne.annee,mois-1,1),new Date(ligne.annee,mois,0),jours_feries);
 										// charger les conges
 										ligne.CPn1=$scope.compterConges(dataConges, mois, "Congé "+($scope.annee-1));
@@ -106,13 +119,14 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 										ligne.RTTn=$scope.compterConges(dataConges, mois, "RTT "+$scope.annee);
 										ligne.maladie=$scope.compterConges(dataConges, mois, "Maladie");
 										ligne.frais_declares=$scope.compterFrais(dataFrais, mois);
-
-										ligne.mois=tmois[mois];
+										if (ligne.tr_distribues===undefined) {
+											ligne.tr_distribues=0;
+										}
+										ligne.tr_a_enlever = $scope.compterTRAEnlever(dataFrais,mois);
 										$scope.listeRecap.push(ligne);
-	
 									}
 									else { // ligne de report
-										ligne=angular.extend(ligne,_.findWhere(listeRecap,{mois:"0"}));
+										ligne=angular.extend(ligne,_.findWhere(listeRecap,{mois:0}));
 										ligne.CPn1=0;
 										ligne.CPn=0;
 										ligne.RTTn1=0;
@@ -121,20 +135,18 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 										ligne.jours_ouvres=0;
 										ligne.tr_a_distribuer=0;
 										ligne.tr_distribues=0;
-										// if(record!==null) {
-										// 	ligne.debit_tr=record.debit_tr;
-										// 	ligne.credit_tr=record.credit_tr;
-										// }
-										// else {
-											//ligne.debit_tr=0;
-											//ligne.credit_tr=0;
-										// }
+										if(ligne.debit_tr===undefined) {
+											ligne.debit_tr=0;
+										}
+										if(ligne.credit_tr===undefined) {
+											ligne.credit_tr=0;
+										}
 										ligne.frais_declares=0;
 										ligne.tr_a_enlever=0;
 										$scope.listeRecap.push(ligne);
 									}
-	
-								}
+								}	//for
+								$scope.recalculerTR(1);
 							});
 	
 					});
@@ -145,18 +157,90 @@ function RecapitulatifCtrl($scope,dataSource,Selection,DateUtils) {
 	
 	$scope.updateRecap = function(i) {
 		var resourceRecap = $scope.resourceRecap;
-		console.log(resourceRecap);
-		var x=resourceRecap.get({id_personne:$scope.personne.id,annee : $scope.annee},function(liste) {
-			console.log(liste);
-		});
-		resourceRecap.save($scope.listeRecap[i]);
-//$scope.listeRecap[i].$save();
-		//console.log(i);
-		//console.log($scope.listeRecap);
-		console.log($scope.listeRecap[i]);
 		
-	}
+		var record=$scope.listeRecap[i];
+		if (i>0) {
+			$scope.recalculerTR(i);
+		}
+		else {
+			//ligne report
+			record.debit_tr = record.debit_tr || 0; // pour forcer à zero si la zone saisie est vide;
+			record.credit_tr = record.credit_tr || 0; // pour forcer à zero si la zone saisie est vide;
+			$scope.recalculerTR(1);
+		}
 
+		// ajout ou modif
+		if ($scope.listeRecap[i].id===undefined) {
+			resourceRecap.add($scope.listeRecap[i],function(x){
+				$scope.listeRecap[i].id=x.id;
+			});
+		}
+		else {
+			resourceRecap.save($scope.listeRecap[i]);
+		}
+
+		
+	}; // updateRecap
+
+	$scope.recalculerTR = function(i) {
+		var record=$scope.listeRecap[i];
+		var mois;
+		var c1,d1,cpn1,cpn,rttn1,rttn,tr_a_enlever;
+		if (i>0) {
+			c1 = $scope.listeRecap[i-1].credit_tr || 0;
+			d1 = $scope.listeRecap[i-1].debit_tr || 0;
+			cpn1 = record.CPn1 || 0;
+			cpn = record.CPn || 0;
+			rttn1 = record.RTTn1 || 0;
+			rttn = record.RTTn || 0;
+			tr_a_enlever = record.tr_a_enlever || 0;
+
+			record.tr_a_distribuer=record.jours_ouvres;
+			record.tr_a_distribuer=record.tr_a_distribuer-Math.floor(cpn1+cpn+rttn1+rttn);
+			record.tr_a_distribuer=record.tr_a_distribuer+c1;
+			record.tr_a_distribuer=record.tr_a_distribuer-d1;
+			record.tr_a_distribuer=record.tr_a_distribuer-tr_a_enlever;
+			if(record.tr_a_distribuer<record.tr_distribues){
+				record.debit_tr=record.tr_distribues-record.tr_a_distribuer;
+				record.credit_tr=0;
+			}
+			else {
+				record.debit_tr=0;
+				record.credit_tr=record.tr_a_distribuer-record.tr_distribues;
+			}
+
+		}
+
+		for (mois=i+1;mois<=12;mois++) {
+			record=$scope.listeRecap[mois];
+			c1 = $scope.listeRecap[mois-1].credit_tr || 0;
+			d1 = $scope.listeRecap[mois-1].debit_tr || 0;
+			cpn1 = record.CPn1 || 0;
+			cpn = record.CPn || 0;
+			rttn1 = record.RTTn1 || 0;
+			rttn = record.RTTn || 0;
+			tr_a_enlever = record.tr_a_enlever || 0;
+			
+			record.tr_a_distribuer=record.jours_ouvres;
+			record.tr_a_distribuer=record.tr_a_distribuer-Math.floor(cpn1+cpn+rttn1+rttn);
+			record.tr_a_distribuer=record.tr_a_distribuer+c1;
+			record.tr_a_distribuer=record.tr_a_distribuer-d1;
+			record.tr_a_distribuer=record.tr_a_distribuer-tr_a_enlever;
+			if(record.tr_a_distribuer<record.tr_distribues){
+				record.debit_tr=record.tr_distribues-record.tr_a_distribuer;
+				record.credit_tr=0;
+			}
+			else {
+				record.debit_tr=0;
+				record.credit_tr=record.tr_a_distribuer-record.tr_distribues;
+			}
+			if(record.tr_a_distribuer<0) {
+				record.tr_a_distribuer = 0;
+			}
+		}
+
+	}; //recalculerTR
+	
 	//initialisation
 	$scope.annee = Selection.annee();
 	$scope.personne=Selection.personne();
